6776aa56ac1b054ec6e53cb5483814b1a46942ae
Dylan Adams <113365542+max5max46@users.noreply.github.com>
2023-01-05 15:25:56 -0400

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(0.0f, 0.01f, 0.0f);
+    }
+}
